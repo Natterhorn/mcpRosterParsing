273ea35ace9de30725979e3cb9bccf49cfaa129e
Natterhorn <Natterhorn@gmail.com>
2022-04-09 00:00:32 -0700

    Adding comments to file

--- roster.py
+++ roster.py
@@ -10,11 +10,16 @@ def findMatch(strVal, printResults):
     if strVal.lower() in nRef.nicknames:
         strVal = nRef.nicknames[strVal.lower()]
 
+    #Remove excel newlines
     compareStr = strVal.replace("_x000D_", "")
+    #Remove hyphen+whitespace at start of string
     compareStr = re.sub("- (?=\w)", "", compareStr)
+    #Remove brackets with characters between them (ex [B], [18])
     compareStr = re.sub("\[(?=\w)\]", "", compareStr)
+    #Remove whitespace from front and back
     compareStr = compareStr.strip()
 
+    #Use most specific scorer first, then try more and more permissive ones
     fuzzyMatch = process.extractOne(compareStr, joinedlist, scorer=ratio, score_cutoff=100)
     sc = 'ratio'
 
@@ -30,6 +35,7 @@ def findMatch(strVal, printResults):
         fuzzyMatch = process.extractOne(compareStr, joinedlist, scorer=partial_ratio, score_cutoff=85)
         sc = 'partial'
 
+    #For testing, print results of the matching
     if printResults == TRUE:
         if fuzzyMatch != None:
             print(compareStr + ":" + fuzzyMatch[0] + "(" + str(fuzzyMatch[1]) + ")|" + sc)
@@ -41,6 +47,7 @@ def findMatch(strVal, printResults):
 # Give the location of the file 
 path = "event_reg.xlsx"
 
+# Join the list of all card names from helper file
 joinedlist = nRef.characters.union(nRef.tactics.union(nRef.extracts.union(nRef.secures)))
 
 #fMatch = findMatch("Cpt America Steve", TRUE)
@@ -52,28 +59,29 @@ sheet_obj = wb.active
 for i in range(2, sheet_obj.max_row+1):
 #for i in range(5, 6):
     sheetName = "Roster " + str(i)
+    # Create a sheet for each roster row
     if sheetName not in wb.sheetnames:
         wb.create_sheet(sheetName)
-
     roster_sheet = wb[sheetName]
 
     cell_obj1 = sheet_obj.cell(row = i, column = 3)  
     j = 1
 
+    # Break up cell contents by newlines, throw each through the fuzzymatcher
     for lineStr in cell_obj1.value.splitlines():
         cell_obj2 = roster_sheet.cell(row = j, column = 1)
 
         match = findMatch(lineStr, FALSE)
         
+        #If a match exists, use that. Otherwise, use unmodified
         if match != None:
             cell_obj2.value = match[0]
         else:
             cell_obj2.value = lineStr
 
+        #Copy the exact value used to a different column to crossreference output
         cell_obj2 = roster_sheet.cell(row = j, column = 11)
         cell_obj2.value = lineStr   
         j += 1
 
 wb.save(path)
-
-
